55b0fa2543f8f4075676f712568aeef50659f2ec
delfos54 <humancaileo@gmail.com>
2026-06-18 20:03:39 -0400

    cxzcxz

--- fidelidad/src/test/java/com/example/fidelidad/FidelidadApplicationTests.java
+++ fidelidad/src/test/java/com/example/fidelidad/FidelidadApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.fidelidad;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FidelidadApplicationTests {
 
 	@Test
