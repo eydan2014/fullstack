03197d922116a6e942f9989c5b070eb7c8c3dbf7
eydan2014 <ey.perez@duocuc.cl>
2026-07-12 16:09:29 -0400

    Delete user directory

--- user/src/main/java/com/example/user/dto/RefreshRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-@Data
-public class RefreshRequest {
-    @NotBlank(message = "El refreshToken es obligatorio")
-    private String refreshToken;
-}
