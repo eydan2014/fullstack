34a3554828cda62df359a4048c68081c466ae053
eydan2014 <ey.perez@duocuc.cl>
2026-07-12 16:07:57 -0400

    Delete ms-cocina directory

--- ms-cocina/src/main/java/com/example/ms_cocina/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_cocina.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
