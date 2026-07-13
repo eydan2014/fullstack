e4470fa79bb3a37c3e509b52a679f3096f8a74f3
eydan2014 <ey.perez@duocuc.cl>
2026-07-12 22:42:55 -0400

    Delete ms-cupones directory

--- ms-cupones/src/main/java/com/example/ms_cupones/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_cupones.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
