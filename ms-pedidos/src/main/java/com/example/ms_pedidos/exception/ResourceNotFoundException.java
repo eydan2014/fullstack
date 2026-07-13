ce200326495ff8d46ce171fa8ced82054ced8eb1
eydan2014 <ey.perez@duocuc.cl>
2026-07-12 22:43:11 -0400

    Delete ms-pedidos directory

--- ms-pedidos/src/main/java/com/example/ms_pedidos/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_pedidos.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- ms-pedidos/src/main/java/com/example/ms_pedidos/repository/DetallePedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms_pedidos.repository;
-
-import com.example.ms_pedidos.model.DetallePedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Integer> {
-}
