d4ff1475f06b8ca6fa7d381013a53e611a20fb90
eydan2014 <ey.perez@duocuc.cl>
2026-07-12 22:42:40 -0400

    Delete ms-cocina directory

--- ms-cocina/target/classes/db/migration/V1__crear_tablas_cocina.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tickets_cocina (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    pedido_id INT NOT NULL,
-    estado VARCHAR(50) NOT NULL,
-    observacion VARCHAR(255),
-    fecha_creacion DATETIME NOT NULL,
-    fecha_actualizacion DATETIME
-);
